37d6d081845b9180ded1f70dde0562eede3291d7
Rellie Balagat <relliebalagat@gmail.com>
2017-12-14 20:11:44 +0800

    if/else javascript

---
+++ mod07-03/js/script.js
@@ -0,0 +1,15 @@
+function identifyThis() {
+	var num = parseInt(document.getElementById("numberInput").value);
+	// console.log(num);
+
+	if(num % 2 == 0) {
+		document.getElementById("theMessage").innerHTML = "It is an even number!";
+		document.getElementById("mainWrapper").style.backgroundColor = "green"
+	} else {
+		document.getElementById("theMessage").innerHTML = "It is an odd number!";
+		document.getElementById("mainWrapper").style.backgroundColor = "pink";
+	}
+
+	document.getElementById("header1").style.color = "white";
+
+}
